21e4b6984f1ecf533d5ce2b1099c587d17cf4812
随意 <136446054+mirrorLikeDream@users.noreply.github.com>
2025-03-02 20:23:11 +0800

    校园助手静态页面

---
+++ assets/use-lazy-render-CIhvoqIW.js
@@ -0,0 +1,1 @@
+import{r as a,k as i}from"./index-BwIqwHK2.js";function u(t){const r=a(!1);return i(t,e=>{e&&(r.value=e)},{immediate:!0}),e=>()=>r.value?e():null}export{u};
---
+++ assets/use-scope-id-D8dlsbjc.js
@@ -0,0 +1,1 @@
+import{au as o}from"./index-BwIqwHK2.js";const t=()=>{var e;const{scopeId:n}=((e=o())==null?void 0:e.vnode)||{};return n?{[n]:""}:null};export{t as u};
